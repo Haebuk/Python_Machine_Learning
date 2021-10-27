801986de31e5e1f64b0917c5340b0788fc58f294
Haebuk <cwryu6252@naver.com>
2021-10-27 19:02:18 +0900

    simpleNet

---
+++ meetDeep/common/functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def sigmoid(a):
+    return 1 / (1 + np.exp(-a))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
